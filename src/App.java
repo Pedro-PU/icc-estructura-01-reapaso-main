667cc0579b9f9b82c41c6bd5518fc5ba8139d2bb
markzuck3rberg <141587136+markzuck3rberg@users.noreply.github.com>
2025-04-22 07:45:40 -0500

    first commit

---
+++ src/App.java
@@ -0,0 +1,14 @@
+
+import Models.Persona;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Persona[] personas = new Persona[] {
+
+        };
+
+        /// TODO: GENERAR INSTNACIA DE CONTROLLE Y ORDENAR EL METODO DESPUES BUSCAR POR
+        /// LA EDAD
+
+    }
+}
